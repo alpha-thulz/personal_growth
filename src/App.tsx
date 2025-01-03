8def61fd453c80b6f720cdf8911145c1b32a4f14
Thulani Tyaphile <tj.tyaphile@gmail.com>
2025-01-03 19:34:29 +0200

    Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { AppNav } from "./components/AppNav.tsx";
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+import Contact from "./pages/Contact.tsx";
+
 export default function App() {
     return (
         <>
-            <h1>Hello World!</h1>
+            <AppNav />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
         </>
     );
 }
