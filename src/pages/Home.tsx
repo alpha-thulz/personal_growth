700d317c6afb48010caa0baf57393a2dd7bb99b3
Thulani Tyaphile <tj.tyaphile@gmail.com>
2025-01-12 14:05:21 +0200

    Change view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,7 @@
 import {useEffect, useState} from "react";
 import client from "../utils/connect.ts";
 import Spinner from "../components/Spinner.tsx";
-import BasicTable from "../components/BasicTable.tsx";
 import Button from "../components/Button.tsx";
-import {Link} from "react-router";
 
 export default function Home() {
     const [isLoading, setIsLoading] = useState(true);
@@ -35,24 +33,25 @@ export default function Home() {
                 <Spinner /> :
                 (
                     <div className="container-fluid">
-                        <BasicTable headings={["Name", "Description", "Create at", "Updated at", "Language"]}>
-                            {data?.map((item: {id: number, name:string, html_url:string, description: string, created_at:string, updated_at:string, language:string}) => (
-                                <tr key={ item.id }>
-                                    <td>
-                                        {
-                                            extractUrls(item.description).length === 0 ? item.name : (
-                                                <Link to={extractUrls(item.description)[0]}>{item.name}</Link>
-                                            )
-                                        }
-                                    </td>
-                                    <td>{ item.description }</td>
-                                    <td>{ new Date(item.created_at).toLocaleString() }</td>
-                                    <td>{ new Date(item.updated_at).toLocaleString() }</td>
-                                    <td>{ item.language }</td>
-                                    <td><Button label="View repo" onClick={() => window.open(item.html_url, "_blank")} /></td>
-                                </tr>
-                            ))}
-                        </BasicTable>
+                        {data?.map((item: {id: number, name:string, html_url:string, description: string, created_at:string, updated_at:string, language:string}) => (
+                            <div className="card text-bg-dark text-center" style={{margin:5}}>
+                                <div className="card-header">
+                                    {item.name} | {item.language}
+                                </div>
+                                <div className="card-body">
+                                    <p>{item.description}</p>
+                                    {(extractUrls(item.description).length !== 0) && (
+                                        <Button label={"Live Demo"} onClick={() => window.open(extractUrls(item.description)[0], "_blank")} color={"info"} />
+                                    )}
+                                    <Button label="View repo" onClick={() => window.open(item.html_url, "_blank")} />
+                                </div>
+                                <div className="card-footer">
+                                    {`Update on ${new Date(item.updated_at).toLocaleString()}`}
+                                    <br/>
+                                    {`Created on ${new Date(item.created_at).toLocaleString()}`}
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 )
             }
